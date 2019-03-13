d4cb6723edebbbbc021b1a80452ed24d662b8790
micro <micro@192.168.1.55>
2019-03-12 21:02:43 -0500

    fecha de modificacion

--- HolaMundo/src/com/sergioh/holamundo/Main.java
+++ HolaMundo/src/com/sergioh/holamundo/Main.java
@@ -1,17 +1,24 @@
 package com.sergioh.holamundo;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Date date=new Date();
 		
 	System.out.println("Re-instalado eclipse: HOLA MUNDO");
 	
 	System.out.println("Numeros del 1 al 100");
 	
-	for (int i = 0; i <= 100 ; i++) {
+	for (int i = 0; i <= 5 ; i++) {
 		System.out.println(i);
 	}
-	System.out.println();
+	System.out.print("Fecha de modificacion actualizada: \n"+date.toLocaleString());
+	
+	
+
 	}
+	
 
 }
