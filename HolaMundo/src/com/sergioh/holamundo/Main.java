ba9bbff6d121b4febd7c2d09897d5f59e55040b1
micro <micro@192.168.1.55>
2019-03-12 21:08:30 -0500

    12-03-2019/fecha modificacion

--- HolaMundo/src/com/sergioh/holamundo/Main.java
+++ HolaMundo/src/com/sergioh/holamundo/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	
 	System.out.println("Numeros del 1 al 100");
 	
-	for (int i = 0; i <= 5 ; i++) {
+	for (int i = 0; i <= 100 ; i++) {
 		System.out.println(i);
 	}
 	System.out.print("Fecha de modificacion actualizada: \n"+date.toLocaleString());
